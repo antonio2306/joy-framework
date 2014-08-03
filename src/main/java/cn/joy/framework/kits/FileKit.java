0c38715320b1e71a80aa6961ce75ef7767b5f381
Administrator <Administrator@liyy-PC>
2014-08-03 17:43:50 +0800

    工具类

--- src/main/java/cn/joy/framework/kits/FileKit.java
+++ src/main/java/cn/joy/framework/kits/FileKit.java
@@ -1,48 +1,110 @@
 package cn.joy.framework.kits;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.RandomAccessFile;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import java.util.Enumeration;
-import java.util.Map;
 
 public class FileKit {
 
-	public static boolean isDirExisted(String dir) {
-		File dirname = new File(dir);
-		return dirname.isDirectory();
-	}
-
-	public static synchronized boolean mkdirs(String filePath) {
-		File file = new File(filePath);
-		if (!file.exists())
-			mkdirs(file);
-		return true;
-	}
-
-	public static synchronized boolean mkdirs(File filePath) {
-		if (!filePath.exists()) {
-			String parent = filePath.getParent();
-			return new File(parent).mkdirs();
-		} else {
-			return false;
-		}
-	}
-
-	public static synchronized void mkRealDir(String dirPath) {
-		File file = new File(dirPath);
-		if (!file.exists()) {
-			String parent = file.getParent();
-			File p = new File(parent);
-			if (!p.exists()) {
-				mkRealDir(parent);
-				mkRealDir(dirPath);
-			} else {
-				file.mkdirs();
+	public void copyFileByNIO(File source, File target) {
+		FileInputStream fi = null;
+		FileOutputStream fo = null;
+		FileChannel in = null;
+		FileChannel out = null;
+		try {
+			fi = new FileInputStream(source);
+			fo = new FileOutputStream(target);
+			in = fi.getChannel();
+			out = fo.getChannel();
+			in.transferTo(0, in.size(), out);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				fi.close();
+				in.close();
+				fo.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 		}
 	}
+	
+	public static void copyFileByNIOBuffer(File source, File target) {  
+		FileInputStream fi = null;
+		FileOutputStream fo = null;
+	    FileChannel in = null;  
+	    FileChannel out = null;  
+	    try {  
+	    	fi = new FileInputStream(source);  
+	    	fo = new FileOutputStream(target);  
+	        in = fi.getChannel();  
+	        out = fo.getChannel();  
+	        ByteBuffer buffer = ByteBuffer.allocate(4096);  
+	        while (in.read(buffer) != -1) {  
+	            buffer.flip();  
+	            out.write(buffer);  
+	            buffer.clear();  
+	        }  
+	    } catch (IOException e) {  
+	        e.printStackTrace();  
+	    } finally {  
+	    	try {
+				fi.close();
+				in.close();
+				fo.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    }  
+	}  
+	
+	public static void copyFile(File source, File target) {  
+	    InputStream fis = null;  
+	    OutputStream fos = null;  
+	    try {  
+	        fis = new BufferedInputStream(new FileInputStream(source));  
+	        fos = new BufferedOutputStream(new FileOutputStream(target));  
+	        byte[] buf = new byte[4096];  
+	        int i;  
+	        while ((i = fis.read(buf)) != -1) {  
+	            fos.write(buf, 0, i);  
+	        }  
+	    }  
+	    catch (Exception e) {  
+	        e.printStackTrace();  
+	    } finally {  
+	    	try {
+	    		fis.close();
+	    		fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+	    }  
+	}  
 
 	public static boolean delete(String path) {
 		return delete(new File(path));
@@ -66,165 +128,11 @@ public class FileKit {
 		return deleted;
 	}
 
-	public static void copy(String src, String target) throws IOException {
-		copy(src, target, null);
-	}
-
-	public static void copy(String src, String target, String excludeExt) throws IOException {
-		copy(src, target, excludeExt, true);
-	}
-
-	public static void copy(String src, String target, String excludeExt, boolean coverExist) throws IOException {
-		File srcFile = new File(src);
-		if (srcFile.exists()) {
-			if (srcFile.isFile()) {
-				if (srcFile.getName().equalsIgnoreCase("Thumbs.db"))
-					return;
-				if (excludeExt != null && srcFile.getName().endsWith("." + excludeExt))
-					return;
-				BufferedOutputStream bos = null;
-				BufferedInputStream bis = null;
-				try {
-					int bytesRead;
-					byte[] buf = new byte[8096];
-
-					mkdirs(src);
-					mkdirs(target);
-
-					File targetFile = new File(target);
-					if (targetFile.exists() && targetFile.isDirectory()) {
-						targetFile = new File(targetFile, srcFile.getName());
-					}
-					if (!coverExist && targetFile.exists() && targetFile.isFile()) {
-						System.out.println("cover=" + coverExist + ", file=" + targetFile.getAbsolutePath()
-								+ ", exist=" + targetFile.exists());
-						return;
-					}
-
-					bos = new BufferedOutputStream(new FileOutputStream(targetFile));
-					bis = new BufferedInputStream(new FileInputStream(new File(src)));
-					while ((bytesRead = bis.read(buf)) >= 0) {
-						bos.write(buf, 0, bytesRead);
-					}
-					bos.flush();
-				} finally {
-					if (bos != null) {
-						bos.close();
-					}
-					if (bis != null) {
-						bis.close();
-					}
-				}
-			} else {
-				// crate tareget dir;
-				new File(target).mkdir();
-
-				// copy sub files and directories;
-				char ch = src.charAt(src.length() - 1);
-				if (ch != '\\' && ch != '/') {
-					src += File.separatorChar;
-				}
-
-				ch = target.charAt(target.length() - 1);
-				if (ch != '\\' && ch != '/') {
-					target += File.separatorChar;
-				}
-
-				String[] subs = srcFile.list();
-				for (int i = 0; i < subs.length; i++) {
-					copy(src + subs[i], target + subs[i], excludeExt, coverExist);
-				}
-			}
-		}
-	}
-
-	public static String replace(String src, String prefix, String suffix, Map props) {
-		int index1;
-		int index2;
-		int len1 = prefix.length();
-		int len2 = suffix.length();
-
-		StringBuffer sb = new StringBuffer();
-
-		index1 = src.indexOf(prefix);
-		while (index1 >= 0) {
-			sb.append(src.substring(0, index1));
-			src = src.substring(index1 + len1);
-			if (src.startsWith(prefix)) {
-				sb.append(prefix);
-				break;
-			} else {
-				index2 = src.indexOf(suffix);
-				if (index2 >= 0) {
-					String t = src.substring(0, index2);
-					Object o = props.get(t);
-					String sp = (o == null ? "" : o.toString());
-					sb.append(sp);
-					src = src.substring((index2 + len2));
-					index1 = src.indexOf(prefix);
-				} else {
-					sb.append(prefix);
-					break;
-				}
-			}
-		}
-		sb.append(src);
-		return new String(sb);
-	}
-
-	public static void replaceFile(String filePath, String prefix, String suffix, Map props) throws IOException {
-		CharArrayWriter caw = new CharArrayWriter(8096);
-		BufferedReader r = new BufferedReader(new FileReader(filePath));
-
-		// read and replace symbols in the file;
-		String line = r.readLine();
-		while (line != null) {
-			String rl = replace(line, prefix, suffix, props);
-			caw.write(rl);
-			caw.write("\n");
-			line = r.readLine();
-		}
-		r.close();
-
-		caw.flush();
-		char[] ca = caw.toCharArray();
-		caw.close();
-
-		// write back the replaced contents to the orinal files;
-		BufferedWriter bw = new BufferedWriter(new FileWriter(filePath));
-		bw.write(ca);
-		bw.flush();
-		bw.close();
-	}
-
-	public static Properties readPropFile(String filePath, String prefix, String suffix, Map props) throws IOException {
-		return readPropFile(new File(filePath), prefix, suffix, props);
-	}
-
-	public static Properties readPropFile(File file, String prefix, String suffix, Map props) throws IOException {
-
-		Properties p = new Properties();
-		p.load(new FileInputStream(file));
-
-		// read and replace symbols in the file;
-		Enumeration enum2 = p.keys();
-		while (enum2.hasMoreElements()) {
-			String key = (String) enum2.nextElement();
-			String value = (String) p.get(key);
-			value = replace(value, prefix, suffix, props);
-			p.setProperty(key, value);
-		}
-
-		// store the replaced properties;
-		return p;
-	}
-
 	public static List<File> filerFiles(String path, String suffix) {
 		return filterFiles(path, suffix, false);
 	}
 
 	public static List<File> filterFiles(String path, String suffix, boolean recusive) {
-		int index = 0;
 		File f = new File(path);
 		if (!f.exists()) {
 			return null;
@@ -232,7 +140,6 @@ public class FileKit {
 			List<File> files = new ArrayList<File>();
 			File[] children = f.listFiles();
 			int count = children.length;
-			File[] is = new File[count];
 			for (int i = 0; i < count; i++) {
 				File mapping = children[i];
 				if (mapping.isFile()) {
@@ -270,15 +177,7 @@ public class FileKit {
 		}
 	}
 
-	public static String getFileName(File file) {
-		if (file == null) {
-			return null;
-		} else {
-			return getFileName(file.getAbsolutePath());
-		}
-	}
-
-	public static String getExtName(String fileName) {
+	public static String getFileExt(String fileName) {
 		int index = fileName.lastIndexOf('.');
 		if (index >= 0) {
 			return fileName.substring(index + 1);
@@ -300,111 +199,214 @@ public class FileKit {
 		}
 	}
 
-	public static String readFirstLineFromFile(String filePath) throws IOException {
-		String info = "-1";
-		mkdirs(filePath);
-		System.out.println("=filePath=" + filePath);
+	public static byte[] readFileBytes(String filePath) throws IOException {
 		File f = new File(filePath);
-		// writeInfoToFileNotAppend("",filePath);
-		RandomAccessFile raf = new RandomAccessFile(f, "r");
-		info = raf.readLine();
-		// info=raf.readUTF();
-		raf.close();
-		System.out.println("==info==" + info);
-		if (info == null) {
-			info = "1";
-			System.out.println("==set info==" + info);
-			writeInfoToFileNotAppend("1", filePath);
+		if (!f.exists()) {
+			throw new FileNotFoundException(filePath);
 		}
-		return info;
 
-	}
-
-	public static boolean writeInfoToFileNotAppend(String info, String filePath) {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream((int) f.length());
+		BufferedInputStream in = null;
 		try {
-			mkdirs(filePath);
-			File f = new File(filePath);
-			FileOutputStream fos = new FileOutputStream(f, false);
-			byte[] b = info.getBytes();
-			fos.write(b);
-			fos.flush();
-			fos.close();
-			return true;
-		} catch (Exception ex) {
-			return false;
-		}
-	}
-
-	public static String readInfoFromFile(String filePath) {
-		StringBuffer sb = new StringBuffer();
-		try {
-			mkdirs(filePath);
-			File f = new File(filePath);
-			RandomAccessFile raf = new RandomAccessFile(f, "r");
-			String s = "";
-			while ((s = raf.readLine()) != null) {
-				sb.append(s + "\r\n");
+			in = new BufferedInputStream(new FileInputStream(f));
+			int buf_size = 1024;
+			byte[] buffer = new byte[buf_size];
+			int len = 0;
+			while (-1 != (len = in.read(buffer, 0, buf_size))) {
+				bos.write(buffer, 0, len);
 			}
-			raf.close();
-		} catch (Exception ex) {
+			return bos.toByteArray();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			try {
+				in.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			bos.close();
+		}
+	}
 
+	public static byte[] readFileBytesByNIO(String filePath) throws IOException {
+		File f = new File(filePath);
+		if (!f.exists()) {
+			throw new FileNotFoundException(filePath);
+		}
+
+		FileChannel channel = null;
+		FileInputStream fs = null;
+		try {
+			fs = new FileInputStream(f);
+			channel = fs.getChannel();
+			ByteBuffer byteBuffer = ByteBuffer.allocate((int) channel.size());
+			while ((channel.read(byteBuffer)) > 0) {
+				// do nothing
+				// System.out.println("reading");
+			}
+			return byteBuffer.array();
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			try {
+				channel.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				fs.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static byte[] readFileBytesByMapped(String filePath) throws IOException {
+		FileChannel fc = null;
+		try {
+			fc = new RandomAccessFile(filePath, "r").getChannel();
+			MappedByteBuffer byteBuffer = fc.map(MapMode.READ_ONLY, 0, fc.size()).load();
+			System.out.println(byteBuffer.isLoaded());
+			byte[] result = new byte[(int) fc.size()];
+			if (byteBuffer.remaining() > 0) {
+				// System.out.println("remain");
+				byteBuffer.get(result, 0, byteBuffer.remaining());
+			}
+			return result;
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			try {
+				fc.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static String readFile(String filePath) throws IOException {
+		File file = new File(filePath);
+		if (!file.exists() || file.isDirectory())
+			throw new FileNotFoundException();
+		String strContent;
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader(file));
+			while ((strContent = br.readLine()) != null) {
+				sb.append(strContent).append("\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		return sb.toString();
 	}
 
-	public static boolean writeInfoToFile(String info, String filePath) {
+	public static String readFile(String filePath, String charset) throws IOException {
+		File file = new File(filePath);
+		if (!file.exists() || file.isDirectory())
+			throw new FileNotFoundException();
+		if (charset == null || charset.length() == 0)
+			charset = "UTF-8";
+		String strContent;
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = null;
 		try {
-			mkdirs(filePath);
-			File f = new File(filePath);
-			FileOutputStream fos = new FileOutputStream(f);
-			byte[] b = info.getBytes();
-			fos.write(b);
+			br = new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+			while ((strContent = br.readLine()) != null) {
+				sb.append(strContent).append("\n");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				br.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
+	}
+	
+	public static void writeInfoToFile(String info, String filePath) {
+		FileOutputStream fos = null;
+		try {
+			fos = new FileOutputStream(new File(filePath));
+			fos.write(info.getBytes());
 			fos.flush();
-			fos.close();
-			return true;
-		} catch (Exception ex) {
-			return false;
+		} catch (Exception e) {
+		} finally {
+			try {
+				fos.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
-	public static void modifyFileCharset(String filepath, String srcset, String targetset) {
-		File file = new File(filepath);
+	public static void writeInfoToFile(String info, String filePath, String charset) {
+		BufferedWriter output = null;
+		try {
+			if (charset == null || charset.length() == 0)
+				charset = "UTF-8";
+			output = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(new File(filePath)), charset));
+			output.write(info);
+		} catch (Exception e) {
+		} finally {
+			try {
+				output.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void changeFileCharset(String filePath, String srcCharset, String targetCharset) {
+		File file = new File(filePath);
 		if (!file.exists())
 			return;
 		if ("\\".equals(File.separator))
 			return;
-		File tmpFile = new File(filepath + ".tmp");
+		File tmpFile = new File(filePath + ".tmp");
 		file.renameTo(tmpFile);
+		
+		InputStream in = null;
+		OutputStream out = null;
+		Reader r = null;
+		Writer w = null;
 		try {
-			InputStream in = new FileInputStream(tmpFile);
-			OutputStream out = new FileOutputStream(file);
-
-			Reader r = new BufferedReader(new InputStreamReader(in, srcset));
-			Writer w = new BufferedWriter(new OutputStreamWriter(out, targetset));
+			in = new FileInputStream(tmpFile);
+			out = new FileOutputStream(file);
+			r = new BufferedReader(new InputStreamReader(in, srcCharset));
+			w = new BufferedWriter(new OutputStreamWriter(out, targetCharset));
 
 			char[] buffer = new char[4096];
 			int len;
 			while ((len = r.read(buffer)) != -1)
 				w.write(buffer, 0, len);
-			r.close();
 			w.flush();
-			w.close();
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				r.close();
+				in.close();
+				w.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		tmpFile.delete();
-	}
-
-	public static boolean rename(File srcFile, File distFile) {
-		if (!srcFile.exists())
-			return false;
-		else {
-			return srcFile.renameTo(distFile);
-		}
-	}
-
-	public static boolean rename(String src, String dist) {
-		return rename(new File(src), new File(dist));
 	}
 
 }
