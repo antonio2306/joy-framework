8bab66ccfee073ef63580ce359356c58ad135f84
Administrator <Administrator@liyy-PC>
2014-09-18 16:52:41 +0800

    版本改为1.1

---
+++ src/main/java/cn/joy/framework/core/JoyParam.java
@@ -0,0 +1,15 @@
+package cn.joy.framework.core;
+
+import cn.joy.framework.core.JoyBundle;
+
+/**
+ * 方法调用的简单参数对象
+ * @author liyy
+ * @date 2014-09-17
+ */
+public class JoyParam extends JoyBundle<JoyParam> {
+	public static JoyParam create(){
+		return new JoyParam();
+	}
+	
+}
