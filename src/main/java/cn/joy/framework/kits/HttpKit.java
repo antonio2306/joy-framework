6e980a6d7fc7253638571f67a53ae362dc53915d
joy <antonio2306@163.com>
2015-08-20 13:40:20 +0800

    HttpKit 获取客户端IP

--- src/main/java/cn/joy/framework/kits/HttpKit.java
+++ src/main/java/cn/joy/framework/kits/HttpKit.java
@@ -316,16 +316,16 @@ public class HttpKit {
 	}
 
 	public static String getClientIP(HttpServletRequest request) {
-		String ipAddress = request.getHeader("X-Forwarded-For");
-		if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-			ipAddress = request.getHeader("Proxy-Client-IP");
-		}
-		if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-			ipAddress = request.getHeader("WL-Proxy-Client-IP");
-		}
-		if (ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
-			ipAddress = request.getRemoteAddr();
-			if (ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")) {
+		String ip = request.getHeader("X-Forwarded-For");
+		if (StringKit.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
+			ip = request.getHeader("Proxy-Client-IP");
+		if (StringKit.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
+			ip = request.getHeader("WL-Proxy-Client-IP");
+		if (StringKit.isEmpty(ip) || "unknown".equalsIgnoreCase(ip))
+			ip = request.getHeader("X-Real-IP");
+		if (StringKit.isEmpty(ip) || "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+			if (ip.equals("127.0.0.1") || ip.equals("0:0:0:0:0:0:0:1")) {
 				// 根据网卡取本机配置的IP
 				InetAddress inet = null;
 				try {
@@ -333,16 +333,15 @@ public class HttpKit {
 				} catch (UnknownHostException e) {
 					e.printStackTrace();
 				}
-				ipAddress = inet.getHostAddress();
+				ip = inet.getHostAddress();
 			}
 		}
 		// 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
-		if (ipAddress != null && ipAddress.length() > 15) { // "***.***.***.***".length()
-															// = 15
-			if (ipAddress.indexOf(",") > 0) {
-				ipAddress = ipAddress.substring(0, ipAddress.indexOf(","));
-			}
+		if (StringKit.isNotEmpty(ip) && ip.length() > 15) { // "***.***.***.***".length() = 15
+			int index = ip.indexOf(",");
+			if (index != -1)
+				ip = ip.substring(0, index);
 		}
-		return ipAddress;
+		return ip;
 	}
 }
