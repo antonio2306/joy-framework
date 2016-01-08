29fc056ee0fd35e0a02d22c27809255a18ebdfc3
yylee <antonio2306@163.com>
2016-01-08 11:08:13 +0800

    cors

--- src/main/java/cn/joy/framework/kits/HttpKit.java
+++ src/main/java/cn/joy/framework/kits/HttpKit.java
@@ -400,4 +400,11 @@ public class HttpKit {
 		}
 		return ip;
 	}
+	
+	public static void setCORS(HttpServletResponse response){
+		response.setHeader("Access-Control-Allow-Origin", "*");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+	    response.setHeader("Access-Control-Max-Age", "3600");
+	    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+	}
 }
